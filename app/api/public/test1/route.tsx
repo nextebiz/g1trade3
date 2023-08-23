c67354f669dc594320a81e2fe7c7ea918be82c43
isavepakistan <isavepak@gmail.com>
2023-08-23 13:34:38 +0500

    m

---
+++ app/api/public/test1/route.tsx
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    const mykey = process.env.JWT_SECRET;
+    return NextResponse.json({
+        msg: "hello", 
+        key: mykey
+    })
+}
