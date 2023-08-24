42695c33053e1feb89aff5b100b768fee3f0540c
isavepakistan <isavepak@gmail.com>
2023-08-25 04:44:02 +0500

    fb

---
+++ app/(public)/social/privacy-policy/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='p-5 text-black bg-white'>
+            <h1 className='text-2xl pb-5'>Privacy Policy</h1>
+            <div>
+                <p className='mb-3'>Application Privacy Statement</p>
+                <p className='mb-3'>This privacy statement (“Privacy Statement”) applies to the treatment of personally identifiable information submitted by, or otherwise obtained from, you in connection with the associated application (“Application”). The Application is provided by G1Trade (and may be provided by G1Trade on behalf of a G1Trade licensor or partner (“Application Partner”). By using or otherwise accessing the Application, you acknowledge that you accept the practices and policies outlined in this Privacy Statement.</p>
+                <p className='mb-3'>WHAT PERSONAL INFORMATION DOES G1Trade COLLECT?</p>
+                <p className='mb-3'>We collect the following types of information from our users:</p>
+                <p className='mb-3'>Personal Information You Provide to Us:<br />
+                    We may receive and store any information you submit to the Application (or otherwise authorize us to obtain – such as, from (for example) your Facebook account). The types of personal information collected may include your full name, email address, gender, IP address, browser information, username, demographic information, and any other information necessary for us to provide the Application services.</p>
+                <p className='mb-3'>Personal Information Collected Automatically:<br />
+                    We receive and store certain types of usage related information whenever you interact with Application. For example, G1Trade may automatically receive and record information regarding your computer’s IP address, browser information, Facebook user ID, Facebook Page fan status, and URLs accessed. Such information may be shared in aggregate (non-personally identifiable) form with our partners.</p>
+                <p className='mb-3'>HOW DOES G1Trade USE THE INFORMATION IT COLLECTS?</p>
+                <p className='mb-3'>G1Trade uses the information described in this Privacy Statement (i) internally, to analyze, develop and improve its products and services, and (ii) as set forth below in the “Will G1Trade Share any of the personal information it Collects” section below.</p>
+                <p className='mb-3'>APPLICATION PARTNER TREATMENT OF PERSONAL INFORMATION.</p>
+                <p className='mb-3'>G1Trade may provide personal information to the applicable Application Partner. The Application Partner’s use of your personal information is subject to the Application Partner’s separate privacy policy – and not this Privacy Statement. The Application Partner’s privacy policy is linked to from within the Partner’s Facebook application.</p>
+                <p className='mb-3'>WILL G1Trade SHARE ANY OF THE PERSONAL INFORMATION IT RECEIVES?</p>
+                <p className='mb-3'>Personal information about our users is an integral part of our business. We neither rent nor sell your personal information to anyone (with the exception of sharing your information with an applicable Application Partner – see the “Application Partner Treatment” section above). We share your personal information only as described below.</p>
+                <p className='mb-3'>Application Partners: We will share your personal information with an applicable Application Partner (see the “Application Partner Treatment” section above).</p>
+                <p className='mb-3'>Agents: We employ other companies and people to perform tasks on our behalf and need to share your information with them to provide products or services to you. Unless we tell you differently, G1Trade’s agents do not have any right to use personal information we share with them beyond what is necessary to assist us. You hereby consent to our sharing of personal information for the above purposes. Business Transfers: In some cases, we may choose to buy or sell assets. In these types of transactions, customer information is typically one of the business assets that are transferred. Moreover, if G1Trade, or substantially all of its assets were acquired, or in the unlikely event that G1Trade goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of G1Trade may continue to use your personal information as set forth in this policy.</p>
+                <p className='mb-3'>Protection of G1Trade and Others: We may release personal information when we believe in good faith that release is necessary to comply with the law; enforce or apply our conditions of use and other agreements; or protect the rights, property, or safety of G1Trade, our employees, our users, or others. This includes exchanging information with other companies and organizations for fraud protection and credit risk reduction.</p>
+                <p className='mb-3'>With Your Consent: Except as set forth above, you will be notified when your personal information may be shared with third parties, and will be able to prevent the sharing of this information.</p>
+                <p className='mb-3'>CONDITIONS OF USE.</p>
+                <p className='mb-3'>If you decide to use or otherwise access the Application, your use/access and any possible dispute over privacy is subject to this Privacy Statement and our Terms of Use, including limitations on damages, arbitration of disputes, and application of California state law.</p>
+                <p className='mb-3'>THIRD PARTY APPLICATIONS/WEBSITES.</p>
+                <p className='mb-3'>The Application may permit you to link to other applications or websites. Such third party applications/websites are not under G1Trade’s control, and such links do not constitute an endorsement by G1Trade of those other applications/websites or the services offered through them. The privacy and security practices of such third party application/websites linked to the Application are not covered by this Privacy Statement, and G1Trade is not responsible for the privacy or security practices or the content of such websites.</p>
+                <p className='mb-3'>WHAT PERSONAL INFORMATION CAN I ACCESS?</p>
+                <p className='mb-3'>G1Trade allows you to access the following information about you for the purpose of viewing, and in certain situations, updating that information. This list may change in the event the Application changes.</p>
+                <p className='mb-3'>- Account and user profile information<br />
+                    - User e-mail address, if applicable<br />
+                    - Facebook profile information, if applicable<br />
+                    - User preferences<br />
+                    - Application specific data</p>
+                <p className='mb-3'>CAN CHILDREN USE THE APPLICATION?</p>
+                <p className='mb-3'>Our site and the services available through G1Trade are not intended for children under the age of 13. G1Trade does not knowingly or specifically collect information about children under the age of 13 and believes that children of any age should get their parents’ consent before giving out any personal information. We encourage you to participate in your child’s web experience.</p>
+                <p className='mb-3'>CHANGES TO THIS PRIVACY STATEMENT.</p>
+                <p className='mb-3'>G1Trade may amend this Privacy Statement from time to time. Use of information we collect now is subject to the Privacy Statement in effect at the time such information is used. If we make changes in the way we use personal information, we will notify you by posting an announcement on our Site or sending you an email. Users are bound by any changes to the Privacy Statement when he or she uses or otherwise accesses the Application after such changes have been first posted.</p>
+                <p className='mb-3'>QUESTIONS OR CONCERNS.</p>
+                <p className='mb-3'>If you have any questions or concerns regarding privacy on our Website, please send us a detailed message at inf[@]g1trade.com. We will make every effort to resolve your concerns.</p>
+                <p className='mb-3'>Effective Date: August 1, 2023</p>
+            </div>
+
+
+        </div>
+    )
+}
