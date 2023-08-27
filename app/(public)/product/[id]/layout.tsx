7e281f9270b693b6c9c70ae58525e860b19229ab
isavepakistan <isavepak@gmail.com>
2023-08-27 16:38:40 +0500

    a

--- app/(public)/product/[id]/layout.tsx
+++ app/(public)/product/[id]/layout.tsx
@@ -13,25 +13,30 @@ interface Props {
 export async function generateMetadata() {
     const headersList = headers()
     const referer = headersList.get('x-invoke-path')
+    console.log("referer")
+    console.log(referer)
     const product_id = referer?.split("/product/").pop() as string;
-
-    const form_data = new FormData();
-    form_data.set("product_id", product_id);
+    console.log("product_id")
     console.log(product_id)
 
-    const api_path = `${process.env.NEXT_PUBLIC_SERVER_PATH}/api/seller/products/find_product`;
-    console.log(api_path)
-    const fetch_product = await fetch(api_path, {
-        method: "POST",
-        body: form_data,
-    });
-    const response_product = await fetch_product.json();
-    const product: Product = response_product.data;
+    if (product_id !== undefined) {
 
-    console.log(product)
-    if (product !== undefined) {
-        return {
-            title: product.title,
+        const form_data = new FormData();
+        form_data.set("product_id", product_id);
+
+        const api_path = `${process.env.NEXT_PUBLIC_SERVER_PATH}/api/seller/products/find_product`;
+        const fetch_product = await fetch(api_path, {
+            method: "POST",
+            body: form_data,
+        });
+        const response_product = await fetch_product.json();
+        const product: Product = response_product.data;
+
+        console.log(product)
+        if (product?.id !== undefined) {
+            return {
+                title: product.title,
+            }
         }
     }
 
